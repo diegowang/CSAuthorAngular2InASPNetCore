413fa53338e32e68deb9111ccebfca3306984421
hwang <hwang@BC1HWANG>
2017-01-26 15:08:52 -0500

    在wwwroot/app下创建目录Login,该目录用来存放LoginComponent

---
+++ CSAuthorAngular2InASPNetCore/ClientApp/app/components/login/login.component.ts
@@ -0,0 +1,34 @@
+﻿import { Component } from "@angular/core";
+import { Router } from '@angular/router';
+
+import { AuthService } from "../../_services/auth.service";
+
+@Component({
+    moduleId: module.id,
+    selector: "my-login",
+    template: require('./login.component.html'),
+    styles: [require('./login.component.css')],
+    providers: [AuthService]
+})
+export class LoginComponent {
+
+    private userName: string;
+    private password: string;
+
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) { }
+
+    login() {
+        this.authService.login(this.userName, this.password)
+            .then(result => {
+                if (result.State == 1) {
+                    this.router.navigate(["./home"]);
+                }
+                else {
+                    alert(result.Msg);
+                }
+            });
+    }
+}
